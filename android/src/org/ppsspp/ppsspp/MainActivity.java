7bc6b69e4e05ce99592dea89ec1d261a6b4d8e15
Sohrab <86171882+SohrabIranpak@users.noreply.github.com>
2021-09-21 23:33:06 -0700

    Delete MainActivity.java

--- android/src/org/ppsspp/ppsspp/MainActivity.java
+++
@@ -1,5 +0,0 @@
-		Intent intent = new Intent();
-		intent.setPackage("org.ppsspp.ppssppgold");
-		intent.setClassName("org.ppsspp.ppssppgold", "org.ppsspp.ppsspp.PpssppActivity");
-		intent.setData(Uri.fromFile(file));
-		startActivity(intent);
