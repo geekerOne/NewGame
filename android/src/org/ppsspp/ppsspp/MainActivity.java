080ff01c6d6ee4e26650bfad315be71abec08b0d
Sohrab <86171882+SohrabIranpak@users.noreply.github.com>
2021-09-24 16:19:33 +0330

    Update MainActivity.java

--- android/src/org/ppsspp/ppsspp/MainActivity.java
+++ android/src/org/ppsspp/ppsspp/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
                 //trying to automate ppsspp
 		///////////////////////////////////////////////////   sdcard0/SandS/example.iso
-		/*
+		
 		copyAssets();
 		
                 String storagePath  = "";
@@ -52,8 +52,7 @@ public class MainActivity extends Activity {
              } catch (IOException e) {
              System.out.println("Can't unzip"); // Or something more intellegent
              }
-	        respondToShortcutRequest(storagePath + "/example.iso");//path of unziped game in MainActivity
-		*/
+		
 		///////////////////////////////////////////////////	
 		// Show file selector dialog here.
 	//	SimpleFileChooser fileDialog = new SimpleFileChooser(this, Environment.getExternalStorageDirectory(), onFileSelectedListener);
@@ -75,3 +74,88 @@ public class MainActivity extends Activity {
 		//respondToShortcutRequest(shortcut_MYParam);
 	}
 }
+
+
+	//should transfer these to main activity
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
+    public void unzipBysomeonegood(String zipFilePath, String destDirectory) throws IOException {
+        File destDir = new File(destDirectory);
+        if (!destDir.exists()) {
+            destDir.mkdir();
+        }
+        ZipInputStream zipIn = new ZipInputStream(new FileInputStream(zipFilePath));
+        ZipEntry entry = zipIn.getNextEntry();
+        // iterates over entries in the zip file
+        while (entry != null) {
+            String filePath = destDirectory + File.separator + entry.getName();
+            if (!entry.isDirectory()) {
+                // if the entry is a file, extracts it
+                extractFile(zipIn, filePath);
+            } else {
+                // if the entry is a directory, make the directory
+                File dir = new File(filePath);
+                dir.mkdirs();
+            }
+            zipIn.closeEntry();
+            entry = zipIn.getNextEntry();
+        }
+        zipIn.close();
+    }
+    /**
+     * Extracts a zip entry (file entry)
+     * @param zipIn
+     * @param filePath
+     * @throws IOException
+     */
+    private void extractFile(ZipInputStream zipIn, String filePath) throws IOException {
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath));
+        byte[] bytesIn = new byte[BUFFER_SIZE];
+        int read = 0;
+        while ((read = zipIn.read(bytesIn)) != -1) {
+            bos.write(bytesIn, 0, read);
+        }
+        bos.close();
+    }
+	
+private void copyAssets()
+{
+      AssetManager assetManager = getAssets();
+      String[] files = null;
+      InputStream in = null;
+      OutputStream out = null;
+      String filename = "game.zip";
+      String storagePath  = "";
+		if (this.getExternalFilesDir(null).getAbsolutePath() != null)
+			storagePath = this.getExternalFilesDir(null).getAbsolutePath();
+		else
+                        storagePath = this.getFilesDir().getAbsolutePath();           
+      try
+      {
+            in = assetManager.open(filename);
+            out = new FileOutputStream(storagePath + "/game.zip");
+            copyFile(in, out);
+            in.close();
+            in = null;
+            out.flush();
+            out.close();
+            out = null;
+      }
+      catch(IOException e)
+      {
+            Log.e("tag", "Failed to copy asset file: " + filename, e);
+      }      
+}
+
+private void copyFile(InputStream in, OutputStream out) throws IOException
+{
+      byte[] buffer = new byte[1024];
+      int read;
+      while((read = in.read(buffer)) != -1)
+      {
+            out.write(buffer, 0, read);
+      }
+}
+/////////////////////////////////////////////////////////////////////////////////	
+
+
+
