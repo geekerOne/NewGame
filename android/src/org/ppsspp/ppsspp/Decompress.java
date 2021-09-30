121d48fab332f01c6f330a4d2ffd57cb684bce7b
Sohrab <86171882+SohrabIranpak@users.noreply.github.com>
2021-09-30 07:34:35 +0330

    Update Decompress.java

--- android/src/org/ppsspp/ppsspp/Decompress.java
+++ android/src/org/ppsspp/ppsspp/Decompress.java
@@ -22,19 +22,51 @@ import android.app.Activity;
 import android.net.Uri;
 import android.content.res.Resources;
 //new
+//new2
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.AlertDialog;
+import android.app.UiModeManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.ActivityInfo;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.ConfigurationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Configuration;
+import android.database.Cursor;
+import android.graphics.PixelFormat;
+import android.media.AudioManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.PowerManager;
+import android.os.Vibrator;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+//new2
+
 public class Decompress extends AsyncTask<Void, Integer, Void> {
 
 	private final static String TAG = "Decompress";
 	private String zipFile;   
 	private String location;
-	
+	private String zipFile2;
+	private String location2;
 	ProgressDialog myProgressDialog;
     Context ctx;
 
-	public Decompress(String zipFile, String location, Context ctx) {
+	public Decompress(String zipFile, String location, String zipFile2, String location2, Context ctx) {
 		super();
 		this.zipFile = zipFile;     
 		this.location = location;
+		this.zipFile2 = zipFile2;     
+		this.location2 = location2;
 		this.ctx = ctx;
 		dirChecker("");   
 	}
@@ -54,9 +86,10 @@ public class Decompress extends AsyncTask<Void, Integer, Void> {
 	protected Void doInBackground(Void... params){
 		int count = 0;
                 int toshow_copy = 0;
+		int toshow_copy2 = 0;
 		try  {
 			
-////////copy//////////////////////////////////////////////////////////////////////////////////////////////	Environment.getExternalStorageDirectory() copy and unzip 2 and check for zip2 after unziping for deleting file	
+////////copy one//////////////////////////////////////////////////////////////////////////////////////////////	Environment.getExternalStorageDirectory() copy and unzip 2 and check for zip2 after unziping for deleting file	
 	    InputStream in = null;
     OutputStream out = null;
     try {
@@ -72,12 +105,12 @@ public class Decompress extends AsyncTask<Void, Integer, Void> {
         byte[] buffer = new byte[1024*10];
         int read;
 	double thePerc_copy = 0;
-        int thegameIsofileSize_copy = 7534208;
+        double thegameIsofileSize_copy = 7534208;
 	//int toshow_copy = 0;
-	int tilNowSize_copy = 0;  
+	double tilNowSize_copy = 0;  
 	read = in.read(buffer);    
         while (read > 0) {
-	tilNowSize_copy += read;
+	tilNowSize_copy += Double.valueOf(read);
 	if(thePerc_copy != (tilNowSize_copy / thegameIsofileSize_copy) * 20) {
         thePerc_copy = (tilNowSize_copy / thegameIsofileSize_copy) * 20;
         toshow_copy = (int)thePerc_copy;  
@@ -101,9 +134,9 @@ public class Decompress extends AsyncTask<Void, Integer, Void> {
         //Toast.makeText(MainActivity.this, "مشکل در کپی کردن", Toast.LENGTH_SHORT).show();
         e.printStackTrace();
     }	    
-////////copy//////////////////////////////////////////////////////////////////////////////////////////////			
+////////copy one//////////////////////////////////////////////////////////////////////////////////////////////						
+////////unzip one///////////////////////////////////////////////////////////////////////////////////////////////////			
 			ZipFile zip = new ZipFile(zipFile);
-			myProgressDialog.setMax(zip.size());
 			FileInputStream fin = new FileInputStream(zipFile);       
 			ZipInputStream zin = new ZipInputStream(fin);
 			ZipEntry ze = null;       
@@ -118,12 +151,12 @@ public class Decompress extends AsyncTask<Void, Integer, Void> {
 					byte[] buffer = new byte[4096 * 8];
 					int len;
 					double thePerc_unzip = 0;
-					int thegameIsofileSize_unzip = 130534208;
+					double thegameIsofileSize_unzip = 130534208;
 					int toshow_unzip = 0;
-					int tilNowSize_unzip = 0;
+					double tilNowSize_unzip = 0;
 					len = zin.read(buffer);
 					while (len > 0) {
-					tilNowSize_unzip += len;
+					tilNowSize_unzip += Double.valueOf(len);
 			              if(thePerc_unzip != (tilNowSize_unzip / thegameIsofileSize_unzip) * 65) {
                                          thePerc_unzip = (tilNowSize_unzip / thegameIsofileSize_unzip) * 65;
                                          toshow_unzip = (int)thePerc_unzip;
@@ -144,6 +177,98 @@ public class Decompress extends AsyncTask<Void, Integer, Void> {
 		} catch(Exception e) {       
 			Log.e("Decompress", "unzip", e);    
 		}    
+////////unzip one////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////		
+////////copy two//////////////////////////////////////////////////////////////////////////////////////////////	Environment.getExternalStorageDirectory() copy and unzip 2 and check for zip2 after unziping for deleting file	
+	    InputStream in_copy2 = null;
+    OutputStream out_copy2 = null;
+    try {
+	//AssetManager asM = ctx.getAssets();
+        //in = asM.open("game.zip");
+	                String storagePath_copy2  = "";
+	//	if (ctx.getExternalFilesDir(null).getAbsolutePath() != null)
+	//		storagePath = ctx.getExternalFilesDir(null).getAbsolutePath();
+	//	else
+                        storagePath = Environment.getExternalStorageDirectory();    
+	in_copy2 = ctx.getResources().openRawResource(R.raw.needs);  
+        out_copy2 = new FileOutputStream(storagePath + "/PSP/needs.zip");
+        byte[] buffer_copy2 = new byte[1024*10];
+        int read_copy2;
+	double thePerc_copy2 = 0;
+        double thegameIsofileSize_copy2 = 7534208;
+	//int toshow_copy = 0;
+	double tilNowSize_copy2 = 0;  
+	read_copy2 = in.read(buffer);    
+        while (read_copy2 > 0) {
+	tilNowSize_copy2 += Double.valueOf(read_copy2);
+	if(thePerc_copy2 != (tilNowSize_copy2 / thegameIsofileSize_copy2) * 20) {
+        thePerc_copy2 = (tilNowSize_copy2 / thegameIsofileSize_copy2) * 20;
+        toshow_copy2 = (int)thePerc_copy2;  
+	publishProgress(toshow_copy2);
+        }   	
+            out_copy2.write(buffer_copy2, 0, read_copy2);
+	    read_copy2 = in_copy2.read(buffer_copy2);    
+
+        }
+        in_copy2.close();
+        in_copy2 = null;
+
+        // write the output file (You have now copied the file)
+        out_copy2.flush();
+        out_copy2.close();
+        out_copy2 = null;
+    } catch (FileNotFoundException e) {
+               // Toast.makeText(MainActivity.this, "مشکل در پیدا کردن فایل", Toast.LENGTH_SHORT).show();
+        e.printStackTrace();
+    } catch (IOException e) {
+        //Toast.makeText(MainActivity.this, "مشکل در کپی کردن", Toast.LENGTH_SHORT).show();
+        e.printStackTrace();
+    }	    
+////////copy two//////////////////////////////////////////////////////////////////////////////////////////////				
+////////unzip two///////////////////////////////////////////////////////////////////////////////////////////////////			
+			ZipFile zip2 = new ZipFile(zipFile2);
+			FileInputStream fin2 = new FileInputStream(zipFile2);       
+			ZipInputStream zin2 = new ZipInputStream(fin2);
+			ZipEntry ze2 = null;       
+			while ((ze2 = zin2.getNextEntry()) != null) {
+
+				Log.v("Decompress", "Unzipping " + ze2.getName());          
+				if(ze2.isDirectory()) {           
+					dirChecker(ze2.getName());         
+				} else {      
+					FileOutputStream fout2 = new FileOutputStream(location2 + ze2.getName());
+					
+					byte[] buffer2 = new byte[4096 * 8];
+					int len2;
+					double thePerc_unzip2 = 0;
+					double thegameIsofileSize_unzip2 = 130534208;
+					int toshow_unzip2 = 0;
+					double tilNowSize_unzip2 = 0;
+					len2 = zin2.read(buffer2);
+					while (len2 > 0) {
+					tilNowSize_unzip2 += Double.valueOf(len2);
+			              if(thePerc_unzip2 != (tilNowSize_unzip2 / thegameIsofileSize_unzip2) * 65) {
+                                         thePerc_unzip2 = (tilNowSize_unzip2 / thegameIsofileSize_unzip2) * 65;
+                                         toshow_unzip2 = (int)thePerc_unzip2;
+					 toshow_unzip2 += toshow_copy2;  
+			                 publishProgress(toshow_unzip2);
+                                         }   
+						fout2.write(buffer2, 0, len2);
+						len2 = zin2.read(buffer2);
+					        //count++;
+						//publishProgress(count);// Here I am doing the update of my progress bar
+					}
+					fout2.close();
+					zin2.closeEntry();
+					
+				}                
+			}       
+			zin2.close();    
+		} catch(Exception e) {       
+			Log.e("Decompress", "unzip", e);    
+		}    
+////////unzip two////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////		
+		
+		
 		return null;
 	}
 
