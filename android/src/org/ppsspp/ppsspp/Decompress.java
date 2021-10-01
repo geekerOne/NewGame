f5d7dbf96f17a40c9907f59988bbb602579feda5
Sohrab <86171882+SohrabIranpak@users.noreply.github.com>
2021-10-01 07:01:35 +0330

    Update Decompress.java

--- android/src/org/ppsspp/ppsspp/Decompress.java
+++ android/src/org/ppsspp/ppsspp/Decompress.java
@@ -241,7 +241,7 @@ public class Decompress extends AsyncTask<Void, Integer, Void> {
 				if(ze2.isDirectory()) {           
 					dirChecker(ze2.getName());         
 				} else {      
-					FileOutputStream fout2 = new FileOutputStream(location2 + ile.separator + ze2.getName());
+					FileOutputStream fout2 = new FileOutputStream(location2 + File.separator + ze2.getName());
 					
 					byte[] buffer2 = new byte[4096 * 8];
 					int len2;
