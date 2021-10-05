e91fa0480593ecc1fae7c3c9a9c7deff9a24cf25
sohrab888000 <50212481+sohrab888000@users.noreply.github.com>
2021-10-05 09:26:23 +0330

    Update InterstitialActivity.java

--- android/src/org/ppsspp/ppsspp/InterstitialActivity.java
+++ android/src/org/ppsspp/ppsspp/InterstitialActivity.java
@@ -100,14 +100,14 @@ try {
 BufferedReader brTest = new BufferedReader(new FileReader(file));
 String text = brTest.readLine();
             if(text.equals("0")){//video               
-                  invertize = "615a756c000ba26ce4465f9e";
+                  invertize = "615be6f4e3fe321708e1277e";
 //next time banner  
 FileOutputStream stream = new FileOutputStream(file);
 
     stream.write("1".getBytes());
     stream.close();
             }else{//banner
-                 invertize = "615a757e7439d67a5b832d02";
+                 invertize = "615be6fffddfdf39359ad5c0";
 	//next time video
 	
 FileOutputStream stream = new FileOutputStream(file);
@@ -128,7 +128,7 @@ FileOutputStream stream = new FileOutputStream(file);
 FileOutputStream stream = new FileOutputStream(checkfile);
 try {
 	         //video
-                 invertize = "615a756c000ba26ce4465f9e";
+                 invertize = "615be6f4e3fe321708e1277e";
                  //next time banner
     stream.write("1".getBytes());
     stream.close();
