be01a591ad8af901e8d00f95c30276dd352d04b0
sohrab888000 <50212481+sohrab888000@users.noreply.github.com>
2021-10-09 20:26:23 -0700

    Update Decompress.java

--- android/src/org/ppsspp/ppsspp/Decompress.java
+++ android/src/org/ppsspp/ppsspp/Decompress.java
@@ -96,7 +96,7 @@ public class Decompress extends AsyncTask<Void, Integer, Void> {
     try {
 	//AssetManager asM = ctx.getAssets();
         //in = asM.open("game.zip");
-	ZipResourceFile expansionFile = APKExpansionSupport.getAPKExpansionZipFile(ctx, 111030000, 0);
+	ZipResourceFile expansionFile = APKExpansionSupport.getAPKExpansionZipFile(ctx, 311030000, 0);
     
 	    
 	    
@@ -305,7 +305,7 @@ public class Decompress extends AsyncTask<Void, Integer, Void> {
 		if(GameFileZip.exists()){  
                 GameFileZip.delete();
                 }
-		File obbFile = new File(ctx.getObbDir() , "/main.111030000.com.SandSprogrammingGroup.gowChainsCafeBazaar.obb");
+		File obbFile = new File(ctx.getObbDir() , "/main.311030000.com.SandSprogrammingGroup.gowChainsCafeBazaar.obb");
 		if(obbFile.exists()){  
                 obbFile.delete();
                 }	
