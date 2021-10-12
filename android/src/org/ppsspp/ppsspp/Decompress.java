fef858d0d3478b282c61cc4a1cb6238e6ac1c476
sohrab888000 <50212481+sohrab888000@users.noreply.github.com>
2021-10-11 20:54:12 -0700

    Update Decompress.java

--- android/src/org/ppsspp/ppsspp/Decompress.java
+++ android/src/org/ppsspp/ppsspp/Decompress.java
@@ -151,7 +151,7 @@ public class Decompress extends AsyncTask<Void, Integer, Void> {
 				if(ze.isDirectory()) {           
 					dirChecker(ze.getName());         
 				} else {      
-					FileOutputStream fout = new FileOutputStream(location + File.separator + ze.getName());
+					FileOutputStream fout = new FileOutputStream(location + ze.getName());
 					
 					byte[] buffer = new byte[4096 * 8];
 					int len;
@@ -249,7 +249,7 @@ public class Decompress extends AsyncTask<Void, Integer, Void> {
 				if(ze2.isDirectory()) {           
 					dirChecker(ze2.getName());         
 				} else {      
-					FileOutputStream fout2 = new FileOutputStream(location2 + File.separator + ze2.getName());
+					FileOutputStream fout2 = new FileOutputStream(location2 + ze2.getName());
 					
 					byte[] buffer2 = new byte[4096 * 8];
 					int len2;
@@ -331,7 +331,7 @@ public class Decompress extends AsyncTask<Void, Integer, Void> {
 	
 	private void dirChecker(String dir)
 	{
-		File f = new File(location + File.separator + dir);
+		File f = new File(location + dir);
 		if(!f.isDirectory())
 		{
 			f.mkdirs();
