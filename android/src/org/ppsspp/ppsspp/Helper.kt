ffa6f569f11a42fcadefc948c53d625fafd0ffc0
sohrab888000 <50212481+sohrab888000@users.noreply.github.com>
2021-10-11 19:56:52 -0700

    Update Helper.kt

--- android/src/org/ppsspp/ppsspp/Helper.kt
+++ android/src/org/ppsspp/ppsspp/Helper.kt
@@ -1,4 +1,4 @@
-package com.draco.ludere.ui
+package org.ppsspp.ppsspp
 import com.jakewharton.processphoenix.ProcessPhoenix
 import android.content.pm.PackageManager
 import android.content.pm.PackageInfo
@@ -51,195 +51,6 @@ import ir.tapsell.plus.TapsellPlus;
 import ir.tapsell.plus.TapsellPlusInitListener
 import ir.tapsell.plus.model.AdNetworkError
 import ir.tapsell.plus.model.AdNetworks
-
-class MainActivity : AppCompatActivity() {
-
-    
-     val BUFFER_SIZE = 4096 * 8
-    
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-	//for publish in pico file and aparat
-	/*
-	val send_email = findViewById(R.id.send_email) as Button
-        send_email.isEnabled = false
-        send_email.visibility = View.GONE
-	val comments = findViewById(R.id.comments) as Button
-        comments.isEnabled = false
-        comments.visibility = View.GONE
-        val game_page = findViewById(R.id.game_page) as Button
-        game_page.isEnabled = false
-        game_page.visibility = View.GONE    
-	*/
-       //for publish in pico file and aparat
-	
-		
-	        TapsellPlus.initialize(this@MainActivity, "mqpjnidoiarlkalehnpkbthgjrnmolrandscilsmmifcjjfccobsfcihqarqqlieocereq" ,
-				       object : TapsellPlusInitListener {
-       
-            override fun onInitializeSuccess(adNetworks : AdNetworks) {
-          //      Log.d("onInitializeSuccess", adNetworks.name())
-            }
-					       override fun onInitializeFailed( adNetworks : AdNetworks,
-						 adNetworkError : AdNetworkError) {
-             //   Log.e("onInitializeFailed", "ad network: " + adNetworks.name() + ", error: " +	adNetworkError.getErrorMessage())
-            }
-        })
-	
-
-	
-	
-	
-	
-	
-	
-
-	
-	
-	        val storagePath: String = (this.getExternalFilesDir(null) ?: this.filesDir).path
-	 
-	    val folder = storagePath
-val f = File(folder, "system")
-f.mkdir()
-/*
-	    val folder1 = storagePath
-val f1 = File(folder1, "PPSSPP")
-f1.mkdir()
-*/	    
-	val folder2 = storagePath + "/system"
-val f2 = File(folder2, "PPSSPP")
-f2.mkdir()
-
-
-//var dir : File = context.getFilesDir().getParentFile()//context.getExternalFilesDir("pending_downloads")
-
-        val file = File(storagePath + "Records.txt")
-        var fileExists = file.exists()
-         if(fileExists){
-         //nothing
-	 }else{
-                 file.writeText("0")
-	 }//init the first time invertisement randomly
-  
-  
-    }
-
-
-    /** Called when the user touches the button */
-    fun start(view: View) {
-	    
-	   val storagePath: String = (this.getExternalFilesDir(null) ?: this.filesDir).path
-        val cfile = File(storagePath + "/example.iso")//diffrent for each game
-        var fileExists = cfile.exists()
-    val bfile = File(storagePath + "/system/PPSSPP/example.zip")
-        var fileExistscheck = bfile.exists()
-	 val dfile = File(storagePath + "/game.zip")
-        var fileExistscheck2 = dfile.exists()
-	val gfile = File(this.getObbDir() + "/main.111030000.com.SandSprogrammingGroup.gowChainsCafeBazaar.obb")    
-	var fileExistscheck3 = gfile.exists()
-
-            
-    if(fileExists){
-            if(fileExistscheck){
-                 bfile.delete()
-              }
-	    if(fileExistscheck2){
-                 dfile.delete()
-              }
-            if(fileExistscheck3){
-                 gfile.delete()
-              }
-    
-	
-	    startActivity(Intent(this@MainActivity, InterstitialActivity::class.java))
-          //          startActivity(Intent(this@MainActivity, GameActivity::class.java))
-
-    
-    
-    } else {
-
-	    
-        // Do something in response to button click
-        val start_the_game_button = findViewById(R.id.start_the_game_button) as Button
-        start_the_game_button.isEnabled = false
-        start_the_game_button.visibility = View.INVISIBLE
-        val comments = findViewById(R.id.comments) as Button
-        comments.isEnabled = false
-        comments.visibility = View.GONE
-        val game_page = findViewById(R.id.game_page) as Button
-        game_page.isEnabled = false
-        game_page.visibility = View.GONE
-        val exit_button = findViewById(R.id.exit_button) as Button
-        exit_button.isEnabled = false
-        exit_button.visibility = View.GONE
-        val send_email = findViewById(R.id.send_email) as Button
-        send_email.isEnabled = false
-        send_email.visibility = View.GONE
-	val group_games_page = findViewById(R.id.group_games_page) as Button
-        group_games_page.isEnabled = false
-        group_games_page.visibility = View.GONE    
-        val relative = findViewById(R.id.relative) as RelativeLayout
-        relative.setBackgroundResource(0)
-        relative.setBackgroundColor(Color.parseColor("#000000"))
-        
-			
-
-				        someTask(this,this).execute()
-		
-        }
-    }
-
-    fun sendMsg(view: View) {
-	/*bazar*/	
-	val openURL = Intent(android.content.Intent.ACTION_EDIT)
-        openURL.data = Uri.parse("bazaar://details?id=com.draco.ludere.fourXfour")
-        openURL.setPackage("com.farsitel.bazaar")
-        startActivity(openURL)
-    }
-
-    fun sendingEmail(view: View) {
-        /*bazar*/
-        val intent = Intent(Intent.ACTION_SENDTO)
-        intent.data = Uri.parse("mailto: 00sohrabiranpak00@gmail.com")        
-	intent.putExtra(Intent.EXTRA_SUBJECT, "نظر دهی")
-        startActivity(intent)
-    }
-
-    fun goToPage(view: View) {
-	/*bazar*/
-	val openURL = Intent(android.content.Intent.ACTION_VIEW)
-        openURL.data = Uri.parse("bazaar://details?id=com.draco.ludere.fourXfour")
-        openURL.setPackage("com.farsitel.bazaar")
-	startActivity(openURL)
-    }
-
-    
-        fun goToGamesPage(view: View) {
-	/*bazar*/
-	val openURL = Intent(android.content.Intent.ACTION_VIEW)
-        openURL.data = Uri.parse("bazaar://collection?slug=by_author&aid=" + "230310009713")
-        openURL.setPackage("com.farsitel.bazaar")
-	startActivity(openURL)
-    }
-    
-    
-    
-    fun exit_game(view: View) {
-        this@MainActivity.finish()
-        exitProcess(0)
-    }
-    /*
-    fun second_start_game(view: View) {
-	val second_start = findViewById(R.id.second_start) as Button
-        second_start.isEnabled = false
-        second_start.visibility = View.GONE
-            val intent = Intent(this, GameActivity::class.java)   
-	    startActivity(intent)
-	    finish() 
-    }
-      */  
         
         /*asynctask new
         */
@@ -267,18 +78,18 @@ f2.mkdir()
 	
 	var prev_download : Double = -1.0
 		val storagePath: String = (context.getExternalFilesDir(null) ?: context.filesDir).path             
-			var ll = 7816696 
-		        var ll2 = 55534208 
-		        var ll_zip = 7816696
-	            	var ll_zip2 = 200534208
+			var ll = 490816696 
+		        var ll2 = 680534208 
+		        var ll_zip = 550816696
+	            	var ll_zip2 = 680534208
 		
 		
 	 	         var ll_download = 1100000000
 				        
 
             var toshoow = 0
-		val zipFilePath = File(storagePath + "/system/PPSSPP/example.zip")
-            val destDirectory = storagePath + "/system/PPSSPP/"
+		val zipFilePath = File(Environment.getExternalStorageDirectory() + "/PSP/psp.zip")
+            val destDirectory = Environment.getExternalStorageDirectory() + "/PSP/"
 	
 		//new zip2
 	val zipFilePath2 = File(storagePath + "/game.zip")
@@ -287,9 +98,14 @@ f2.mkdir()
 		
 	        val myProgressDialog = ProgressDialog(context)
 //for copy
-    val afile = context.assets.open( "example.zip" )
+    //val afile = context.assets.open( "example.zip" )
     //    val afile2 = context.assets.open( "game.zip" )
-    val bfile = File(storagePath + "/system/PPSSPP/example.zip")	
+    
+    	val folder2 = Environment.getExternalStorageDirectory()
+        val f2 = File(folder2, "PSP")
+        f2.mkdir()
+    
+    val bfile = File(Environment.getExternalStorageDirectory() + "/PSP/psp.zip")	
     val dfile = File(storagePath + "/game.zip")	
             
      override fun onPreExecute() {
@@ -310,12 +126,14 @@ f2.mkdir()
             
             override fun doInBackground(vararg params: Void):String? {
 	
-		    
+	           //for testing obb
+   	val expansionFile =
+        APKExpansionSupport.getAPKExpansionZipFile(context, 111030000, 0)	    
     
 		    //copy1
     var inStream: InputStream? = null
     var outStream: OutputStream? = null
-    inStream = afile       
+    inStream = expansionFile.getInputStream("main/psp.zip")
     outStream = FileOutputStream(bfile)
     val buffer = ByteArray(1024*10)
     var length = inStream.read(buffer)
@@ -335,12 +153,7 @@ f2.mkdir()
     //copy1
     
 
-    
-    
-    
-           //for testing obb
-   	val expansionFile =
-        APKExpansionSupport.getAPKExpansionZipFile(context, 6, 0)
+
 
     		    //copy2
     var inStream2: InputStream? = null
@@ -540,7 +353,7 @@ f2.mkdir()
 			myProgressDialog.dismiss()
 		}
        
-    val bfile = File(storagePath + "/system/PPSSPP/example.zip")	
+    val bfile = File(Environment.getExternalStorageDirectory() + "/PSP/psp.zip")	
             var fileExistscheck = bfile.exists()
             if(fileExistscheck){
               bfile.delete()
@@ -552,7 +365,7 @@ f2.mkdir()
               dfile.delete()
               }
 	    
-    val gfile = File(context.getObbDir() + "/main.111030000.com.SandSprogrammingGroup.gowChainsCafeBazaar.obb")    
+    val gfile = File(context.getObbDir() + "/main.111030000.com.SandSprogrammingGroup.pes2022.obb")    
 	    var fileExistscheck4 = gfile.exists()
             if(fileExistscheck4){
               gfile.delete()
