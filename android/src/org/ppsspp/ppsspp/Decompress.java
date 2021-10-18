067110d5cb15bf27499ea9fba106e999e0ab4e03
sohrab888000 <50212481+sohrab888000@users.noreply.github.com>
2021-10-17 20:07:17 -0700

    Update Decompress.java

--- android/src/org/ppsspp/ppsspp/Decompress.java
+++ android/src/org/ppsspp/ppsspp/Decompress.java
@@ -172,8 +172,7 @@ public class Decompress extends AsyncTask<Void, Integer, Void> {
 					
 					byte[] buffer = new byte[4096 * 8];
 					int len;
-					len = zin.read(buffer);
-					while (len > 0) {
+					while ((len = zin.read(buffer)) != -1) {
 					tilNowSize_unzip += Double.valueOf(len);
 			              if(thePerc_unzip != (tilNowSize_unzip / thegameIsofileSize_unzip) * 20) {
                                          thePerc_unzip = (tilNowSize_unzip / thegameIsofileSize_unzip) * 20;
@@ -182,7 +181,6 @@ public class Decompress extends AsyncTask<Void, Integer, Void> {
 			                 publishProgress(toshow_unzip);
                                          }   
 						fout.write(buffer, 0, len);
-						len = zin.read(buffer);
 					        //count++;
 						//publishProgress(count);// Here I am doing the update of my progress bar
 					}
@@ -273,8 +271,7 @@ public class Decompress extends AsyncTask<Void, Integer, Void> {
 					
 					byte[] buffer2 = new byte[4096 * 8];
 					int len2;
-					len2 = zin2.read(buffer2);
-					while (len2 > 0) {
+					while ((len = zin.read(buffer)) != -1) {
 					tilNowSize_unzip2 += Double.valueOf(len2);
 			              if(thePerc_unzip2 != (tilNowSize_unzip2 / thegameIsofileSize_unzip2) * 20) {
                                          thePerc_unzip2 = (tilNowSize_unzip2 / thegameIsofileSize_unzip2) * 20;
@@ -283,7 +280,6 @@ public class Decompress extends AsyncTask<Void, Integer, Void> {
 			                 publishProgress(toshow_unzip2);
                                          }   
 						fout2.write(buffer2, 0, len2);
-						len2 = zin2.read(buffer2);
 					        //count++;
 						//publishProgress(count);// Here I am doing the update of my progress bar
 					}
